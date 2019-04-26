037bdae339f5f884e622471d95519ce382d6db2b
owlgirl07 <jessica.alyssa.07@live.ca>
2019-04-26 11:13:48 -0300

    more ReadMe Updates

---
+++ GenericExercise/GenericExercise/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GenericExercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFindScore = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFindScore
+            // 
+            this.btnFindScore.Location = new System.Drawing.Point(124, 100);
+            this.btnFindScore.Name = "btnFindScore";
+            this.btnFindScore.Size = new System.Drawing.Size(75, 23);
+            this.btnFindScore.TabIndex = 0;
+            this.btnFindScore.Text = "&Find Grade:";
+            this.btnFindScore.UseVisualStyleBackColor = true;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(27, 48);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(38, 13);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(99, 41);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 201);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnFindScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFindScore;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
+    }
+}
+
